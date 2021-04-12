2527d82e493388a87dbc2356526a41df2e4899ba
Adam Dicken <adam.dicken@bbc.co.uk>
2021-04-12 12:25:56 +0100

    boto should be present already

--- setup.py
+++ setup.py
@@ -80,10 +80,10 @@ setup(
     zip_safe=True,
     platforms='any',
     install_requires=[
-        # 'apache-airflow>=1.10.0',
+        # 'apache-airflow>=1.10.0', # should be present
         # FAB is pulled in from Airflow
         'jwcrypto~=0.6.0',
-        'boto3>=1.16.23'
+        'boto3'
     ],
     setup_requires=[
         'pytest-runner~=4.0',
